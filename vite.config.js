feee8a7cf9bf3d0cf42464fd6b79c218f40bc3e8
drissBrtn <drissabpro@gmail.com>
2025-07-11 18:25:11 +0200

    Fix: Add base path to vite.config.js for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/drissbrtn.github2.io/', // <--- ASSUREZ-VOUS QUE CETTE LIGNE EST PRÉSENTE ET CORRECTE
   plugins: [react( ),tailwindcss()],
   resolve: {
     alias: {
